a4556e115d5db57759b7b6289a0c14e0909f10ff
Andreas <ciscoheat@gmail.com>
2016-05-26 22:45:51 +0200

    Structure and code updates.

--- fdrun.bat
+++ fdrun.bat
@@ -1,4 +1,9 @@
 @echo off
-cd bin
-neko nodeproject.n
+
+:: To run normally:
+neko bin\nodeproject.n
+
+:: To test with travix (initialize with "haxelib dev nodeproject .")
+::haxelib run travix neko
+
 pause
